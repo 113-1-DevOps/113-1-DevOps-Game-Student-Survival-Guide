e681c1fd47bcc7e57f31cde84e3aed04f879a108
StarrySkyYX <132223842+StarrySkyYX@users.noreply.github.com>
2024-11-15 11:54:15 +0800

    完成首頁、轉場

---
+++ config/constants.py
@@ -0,0 +1,16 @@
+# Window Size
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 450
+
+# View Identification
+HOME = 0
+GAME_OVER = 1
+NEXT_INFORM =2
+
+# Game Identification
+TYPHOON = 'W'
+TEACHEROUT = 'T'
+CAR_ACCIDENT = 'C'
+
+# Function Identification
+START  = 'S'
+RETRY = 'R'
