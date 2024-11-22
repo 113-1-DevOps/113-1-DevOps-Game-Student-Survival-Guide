6dbb13df9e5a44687bc6c106d39e0b57989dc578
Yusi0114 <132541872+Yusi0114@users.noreply.github.com>
2024-11-22 08:17:31 +0800

    Update game_model.py

--- src/models/game_model.py
+++ src/models/game_model.py
@@ -16,7 +16,15 @@ class GameModel:
     def __init__(self):
         self.levels = [TYPHOON,TEACHEROUT,CAR_ACCIDENT]
         self.current_level = None
+        self.test_mode = True  # 設置測試模式開關
 
     def start_random_level(self):
-        self.current_level = random.choice(self.levels)
+        # self.current_level = random.choice(self.levels)
+        # return self.current_level
+        if self.test_mode:
+            # 測試模式啟用，第一個關卡固定為颱風
+            self.current_level = TYPHOON
+        else:
+            # 隨機選擇關卡
+            self.current_level = random.choice(self.levels)
         return self.current_level
