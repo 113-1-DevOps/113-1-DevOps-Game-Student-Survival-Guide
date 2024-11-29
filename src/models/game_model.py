21d062a4b37f111a5c14e07c34b74f875cb62765
Yusi0114 <132541872+Yusi0114@users.noreply.github.com>
2024-11-29 10:41:18 +0800

    Update game_model.py
    解決衝突，刪除測試部分

--- src/models/game_model.py
+++ src/models/game_model.py
@@ -16,15 +16,7 @@ class GameModel:
     def __init__(self):
         self.levels = [TYPHOON,TEACHEROUT,CAR_ACCIDENT]
         self.current_level = None
-        self.test_mode = False  # 設置測試模式開關
 
     def start_random_level(self):
-        # self.current_level = random.choice(self.levels)
-        # return self.current_level
-        if self.test_mode:
-            # 方便測試固定關卡用(可刪)
-            self.current_level = TYPHOON
-        else:
-            # 隨機選擇關卡
-            self.current_level = random.choice(self.levels)
+        self.current_level = random.choice(self.levels)
         return self.current_level
